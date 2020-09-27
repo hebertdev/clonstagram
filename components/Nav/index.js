2ca1d27eb6f00262edcf2d6f13cf8b57f352a605
hebertdev <hebert.villafuerte.c@hotmail.com>
2020-09-26 22:07:47 -0500

    add image

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -8,6 +8,7 @@ export default function Nav({ usuario, logout, OpenModalUpload }) {
     <header className="Header__principal">
       <Head>
         <meta charset="utf-8" />
+        <title>Un clon de Instagram ♥ </title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
@@ -17,18 +18,8 @@ export default function Nav({ usuario, logout, OpenModalUpload }) {
         <meta name="keywords" content="Keywords" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
+        <link href="/insta.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/insta.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
