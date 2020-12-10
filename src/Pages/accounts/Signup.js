b4cd62b8aaddcbb713790ec22c1bc05f37627b66
hebert villafuerte <hebert.villafuerte.c@hotmail.com>
2020-12-10 14:48:01 -0500

    error signup

--- src/Pages/accounts/Signup.js
+++ src/Pages/accounts/Signup.js
@@ -11,6 +11,14 @@ import LoginImage from "../../Images/iconos/login_image.png";
 import Container from "../../Components/Container";
 
 export default function Signup({ signup, mostrarAlerta, history }) {
+  const [emailError, setEmailError] = useState(null);
+  const [usernameError, setUsernameError] = useState(null);
+  const [nameError, setNameError] = useState(null);
+  const [lastnameError, setLastnameError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [passwordConfError, setPasswordConfError] = useState(null);
+  const [nonFieldsError, setNonFieldsError] = useState(null);
+
   const [newUser, setNewUser] = useState({
     email: "",
     username: "",
@@ -32,10 +40,15 @@ export default function Signup({ signup, mostrarAlerta, history }) {
       history.push("/accounts/login");
     } catch (error) {
       console.log(error.response.data);
-      mostrarAlerta(
-        "Hubo un problema al registrarse , intente de nuevo.",
-        true
-      );
+      mostrarAlerta("Hubo un problema al registrarse intente de nuevo.", true);
+      console.log(error.response.data.email);
+      setEmailError(error.response.data.email);
+      setUsernameError(error.response.data.username);
+      setNameError(error.response.data.first_name);
+      setLastnameError(error.response.data.last_name);
+      setPasswordError(error.response.data.password);
+      setPasswordConfError(error.response.data.password_confirmation);
+      setNonFieldsError(error.response.data.non_field_errors);
     }
   }
 
@@ -49,54 +62,99 @@ export default function Signup({ signup, mostrarAlerta, history }) {
           <form className="ContainerLogin__right-form" onSubmit={handleSubmit}>
             <h1 className="title__center">Crea una cuenta</h1>
             <br />
+            {emailError && (
+              <small className="error__input"> {emailError} </small>
+            )}
             <input
-              className="input__email"
+              className={`${
+                emailError ? "input__email input_error" : "input__email"
+              }`}
               type="email"
               placeholder="Email"
               required
               name="email"
               onChange={handleInputChange}
+              value={usuario.email}
             />
+
+            {usernameError && (
+              <small className="error__input"> {usernameError} </small>
+            )}
             <input
-              className="input__email"
+              className={`${
+                usernameError ? "input__email input_error" : "input__email"
+              }`}
               type="text"
               placeholder="username"
               required
               name="username"
               onChange={handleInputChange}
+              value={usuario.username}
             />
+
+            {nameError && <small className="error__input"> {nameError} </small>}
             <input
-              className="input__email"
+              className={`${
+                nameError ? "input__email input_error" : "input__email"
+              }`}
               type="text"
               placeholder="First Name"
               required
               name="first_name"
               onChange={handleInputChange}
+              value={usuario.first_name}
             />
+
+            {lastnameError && (
+              <small className="error__input"> {lastnameError} </small>
+            )}
             <input
-              className="input__email"
+              className={`${
+                lastnameError ? "input__email input_error" : "input__email"
+              }`}
               type="text"
               placeholder="Last Name"
               required
               name="last_name"
               onChange={handleInputChange}
+              value={usuario.last_name}
             />
+
+            {passwordError && (
+              <small className="error__input"> {passwordError} </small>
+            )}
             <input
-              className="input__email"
+              className={`${
+                passwordError ? "input__email input_error" : "input__email"
+              }`}
               type="password"
               placeholder="password"
               required
               name="password"
               onChange={handleInputChange}
+              value={usuario.password}
             />
+
+            {passwordConfError && (
+              <small className="error__input"> {passwordConfError} </small>
+            )}
+            {nonFieldsError && (
+              <small className="error__input"> {nonFieldsError} </small>
+            )}
             <input
-              className="input__email"
+              className={`${
+                passwordConfError || nonFieldsError
+                  ? "input__email input_error"
+                  : "input__email"
+              }`}
               type="password"
               placeholder="password"
               required
               name="password_confirmation"
               onChange={handleInputChange}
+              value={usuario.password_confirmation}
             />
+
             <button type="submit" className="btn__login">
               Crear cuenta
             </button>
